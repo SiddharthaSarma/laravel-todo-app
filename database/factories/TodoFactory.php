6197662f2ffaa76284528983b873f739d2149a0a
siddharthasarma <sidduinevitable@gmail.com>
2017-09-16 22:58:51 +0530

    Seeded the database

--- database/factories/TodoFactory.php
+++ database/factories/TodoFactory.php
@@ -13,13 +13,9 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
-    static $password;
-
-    return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
-    ];
+$factory->define(App\Todo::class, function(Faker $faker) {
+	return [
+		'todo' => $faker->sentence(10)
+	];
 });
+
